6e0211c194c9206be5220f3fff0cd48e09123a9a
owsik <jakubolszewski1@gmail.com>
2016-10-17 20:04:07 +0200

    messing aroud

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: '<h1>My First wefwe App</h1>'
+  template: '<h1 (click)="addLike()">My First {{likes}} App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+	likes: number = 0;
+
+	addLike() {
+		this.likes += 1;
+	}
+}
